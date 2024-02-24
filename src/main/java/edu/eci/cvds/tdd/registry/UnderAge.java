998d9958f46ced163be36481ce12274cecae16ee
unknown <juan.sanchez-mo@labinfo.is.escuelaing.edu.co>
2024-02-24 09:56:36 -0500

    Parcial1

--- src/main/java/edu/eci/cvds/tdd/registry/UnderAge.java
+++ src/main/java/edu/eci/cvds/tdd/registry/UnderAge.java
@@ -3,7 +3,6 @@ package edu.eci.cvds.tdd.registry;
 class PersonaEdadMinima {
 
     public boolean EdadMinima (int age) throws Exception {
-        /*
         if (18 <= age && age <= 160) {
             return false;
         } else if(0 <= age && age <= 17){
@@ -11,9 +10,6 @@ class PersonaEdadMinima {
         }else {
             throw new Exception();
         }
-
-         */
-        return false;
-    }
+    
     }
 
